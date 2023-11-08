c85904ee2a8966a57a6819fbbdc7690784d26652
preethipa <143929450+Preethipa19@users.noreply.github.com>
2023-11-08 18:45:36 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,72 @@
+﻿// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "Which of the following statement(s) about stack data structure is/are NOT correct??",
+        answer: "Stack is the FIFO data structure",
+        options: [
+            "Top of the stack always contain the new node",
+            "Stack is the FIFO data structure",
+            "NUll link is present in the last node at the bottom of the stack",
+            "Linked list are used for implementing Stacks"
+        ]
+    },
+    {
+        numb: 2,
+        question: " The data structure required for Breadth First Traversal on a graph is?",
+        answer:"Queue",
+        options: [
+            "Array",
+            "Stack",
+            "Tree",
+            "Queue"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which data structure is used for implementing recursion?",
+        answer: "Stack",
+        options: [
+            "Stack",
+            "Queue",
+            "List",
+            "Array"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which of the following tree data structures is not a balanced binary tree?",
+        answer: "B-tree",
+        options: [
+            "Splay tree",
+            "B-tree",
+            "AVL Tree",
+            "Red-black tree"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What is the advantage of a hash table as a data structure?",
+        answer: "faster access of data",
+        options: [
+            "easy to implement",
+            "faster access of data",
+            "exhibit good locality of reference",
+            "very efficient for less number of entries"
+        ]
+    },
+    // you can uncomment the below codes and make duplicate as more as you want to add question
+    // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    //   {
+    //   numb: 6,
+    //   question: "Your Question is Here",
+    //   answer: "Correct answer of the question is here",
+    //   options: [
+    //     "Option 1",
+    //     "option 2",
+    //     "option 3",
+    //     "option 4"
+    //   ]
+    // },
+];
